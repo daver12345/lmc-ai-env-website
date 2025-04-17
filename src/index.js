10d387f281c48f9c314996f8c4d877296be253d7
daver_12345 <davidbeugnon@hotmail.com>
2025-04-17 17:40:00 -0400

    Used lazy loading to decrease app size

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Why from './pages/Why';
-import SuggestionsPage from './pages/SuggestionsPage';
 import LoadOnTop from './LoadOnTop';
 import './App.css';
+
+const App             = lazy(() => import('./App'));
+const Why             = lazy(() => import('./pages/Why'));
+const SuggestionsPage = lazy(() => import('./pages/SuggestionsPage'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <LoadOnTop />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/why" element={<Why />} />
-        <Route path="/suggestions" element={<SuggestionsPage />} />
-      </Routes>
+      {/* wrap lazy routes in a Suspense boundary */}
+      <Suspense fallback={<div>Loading…</div>}>
+        <Routes>
+          <Route path="/"           element={<App />} />
+          <Route path="/why"        element={<Why />} />
+          <Route path="/suggestions" element={<SuggestionsPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   </React.StrictMode>
 );
