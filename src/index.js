2e2b5c9ea28ae7d889f377f6a378626d5a8fcc90
daver_12345 <davidbeugnon@hotmail.com>
2025-04-17 17:42:44 -0400

    undid lazy loading because it broke css stuff and didn't fix deploy issue

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Why from './pages/Why';
+import SuggestionsPage from './pages/SuggestionsPage';
 import LoadOnTop from './LoadOnTop';
 import './App.css';
-
-const App             = lazy(() => import('./App'));
-const Why             = lazy(() => import('./pages/Why'));
-const SuggestionsPage = lazy(() => import('./pages/SuggestionsPage'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <LoadOnTop />
-      {/* wrap lazy routes in a Suspense boundary */}
-      <Suspense fallback={<div>Loading…</div>}>
-        <Routes>
-          <Route path="/"           element={<App />} />
-          <Route path="/why"        element={<Why />} />
-          <Route path="/suggestions" element={<SuggestionsPage />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/why" element={<Why />} />
+        <Route path="/suggestions" element={<SuggestionsPage />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
